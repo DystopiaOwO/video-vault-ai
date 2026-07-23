5f8909c11398affcc23d8771e6c6cd18bb2b9d1c
DystopiaOwO <34229115+DystopiaOwO@users.noreply.github.com>
2026-07-23 10:57:32 +0800

    Avoid duplicate application mounting

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,1 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
-import "./styles.css";
-
 export { App } from "./App";
-
-const rootElement = document.getElementById("root");
-if (rootElement) createRoot(rootElement).render(<StrictMode><App /></StrictMode>);
