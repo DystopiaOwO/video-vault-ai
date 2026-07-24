30342b73f7a759e6aa290845e0074369430b66ea
DystopiaOwO <34229115+DystopiaOwO@users.noreply.github.com>
2026-07-24 21:29:55 +0800

    fix: restore clean CI optimization file contents

--- tests/test_color_consistency.py
+++ tests/test_color_consistency.py
@@ -1,10 +1,3 @@
-MethodException: 
-Line |
-   2 |  … ency.py')); $text = $text.Replace(([char]13 + [char]10), [char]10); [ …
-     |                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     | Cannot convert argument "oldChar", with value: "
-", for "Replace" to type "System.Char": "Cannot convert value "
-" to type "System.Char". Error: "String must be exactly one character long.""
 from pathlib import Path
 import json
 import subprocess
